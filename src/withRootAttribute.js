c8a10f0eb8ec84df0ebbccca49a77d63052d4e2c
Alexey Vasiliev <leopard.not.a@gmail.com>
2019-04-10 02:09:30 +0300

    code set attributes

--- src/withRootAttribute.js
+++ src/withRootAttribute.js
@@ -3,8 +3,29 @@ import {EVENTS, PARAM_KEY} from './constants';
 import {addons, makeDecorator} from '@storybook/addons';
 
 const updateRootAttribute = (currentOptions) => {
-  console.log('currentOptions', currentOptions)
-  // document.documentElement.classList.add(attributeChange.value);
+  const element = ((root) => {
+    if (root === 'body') {
+      return document.body;
+    }
+    return document.documentElement;
+  })(currentOptions.root)
+
+  if (currentOptions.attribute === 'class') {
+    element.className = '';
+
+    if (currentOptions.currentState.value !== null) {
+      element.classList.add(currentOptions.currentState.value);
+    }
+  } else {
+    element.removeAttribute(currentOptions.attribute);
+
+    if (currentOptions.currentState.value !== null) {
+      element.setAttribute(
+        currentOptions.attribute,
+        currentOptions.currentState.value
+      );
+    }
+  }
 }
 
 export const withRootAttribute = makeDecorator({
