40b998cb19df92b03cfd22cac968850e4e7002e4
Alexey Vasiliev <leopard.not.a@gmail.com>
2026-04-26 17:31:57 +0300

    publish from ga

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
-import {eslint} from 'rollup-plugin-eslint';
 
 const rollupOptions = {
   plugins: [
